daa38919efd68b5ff00240750c5e36c4074c112d
Plaban <turjoyplaban09@gmail.com>
2024-03-20 08:45:55 +0600

    Changes

---
+++ Web Lab Test/myshop/ASP/Home.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p> 
+                    <asp:Label ID="La" runat="server" Text="Bank Account Form:"></asp:Label>   
+             </p>
+            <asp:Label ID="Label1" runat="server" Text="Amount:"></asp:Label>   
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Diposit" OnClick="diposit_btn" />
+            <asp:Button ID="Button2" runat="server" Text="Withdraw" OnClick="withdraw_btn"  />
+            <p> 
+                <asp:Label ID="Label2" runat="server" Text="Current balance: "></asp:Label>  
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            </p>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ Web Lab Test/myshop/ASP/Home.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        BankAccount acc = new BankAccount(1212,0);
+          
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void diposit_btn(object sender, EventArgs e)
+        { 
+            if(TextBox1.Text != string.Empty)
+            {
+                if (Convert.ToDouble(TextBox1.Text )  >= 1000)
+            {
+                Response.Write("Give less than 1000");
+            }
+            else
+            {
+                acc.diposit(Convert.ToDouble(TextBox1.Text));
+                TextBox2.Text = Convert.ToString(acc.showBalance());
+            }
+            }
+            else
+            {
+                Response.Write("Give Diposit Value");
+            }
+
+
+        }
+        protected void withdraw_btn(object sender, EventArgs e)
+        {
+            if(TextBox1.Text != string.Empty)
+            {
+            if ((acc.showBalance() - Convert.ToDouble(TextBox1.Text)) >= 0)
+            {
+                acc.withdraw(Convert.ToDouble(TextBox1.Text));
+                TextBox2.Text = Convert.ToString(acc.showBalance()); 
+            }
+            else
+            {
+                Response.Write("Not Available Balance");
+            }
+            }
+            else
+            {
+                Response.Write("Give Withdraw Value");
+            }
+
+
+             
+        }
+    }
+}
